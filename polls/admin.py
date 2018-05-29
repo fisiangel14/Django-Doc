80ab63c5339dbd73c0f191b5d70a436f9aab5182
Angel Alegre <fisiangel.14@gmail.com>
2018-05-29 12:28:05 -0500

    7.modificando a travez del admin de django

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Question, Choice
 
 # Register your models here.
+
+admin.site.register(Question)
+admin.site.register(Choice)
+
+
+#6.Despues de crear un usuario para el sitio de administracion
+#7.Debemos registrar aqui nuestras app, para su modificacion
