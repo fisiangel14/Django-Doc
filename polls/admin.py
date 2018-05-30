8df3603ab67337e438dee02c3ea29a258b66b4c5
Angel Alegre <fisiangel.14@gmail.com>
2018-05-30 00:15:42 -0500

    9.Manejando el admin de django

--- polls/admin.py
+++ polls/admin.py
@@ -6,6 +6,22 @@ from .models import Question, Choice
 
 #6.Despues de crear un usuario para el sitio de administracion
 #7.Debemos registrar aqui nuestras app, para su modificacion
+#congiuramos mas el sitio de admin
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    fieldsets = [
+        ('Question',         {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
